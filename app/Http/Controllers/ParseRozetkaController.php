f0bb1735c5be529dc56b19795fbd3f0b1fc1f00f
SimaVitalay <simachev28@gmail.com>
2023-05-18 12:02:34 +0300

    Тестовое добавление в БД

--- app/Http/Controllers/ParseRozetkaController.php
+++ app/Http/Controllers/ParseRozetkaController.php
@@ -11,28 +11,70 @@ class ParseRozetkaController extends Controller
     {
         $client = new Client();
 
-        $request = $client->get('https://xl-catalog-api.rozetka.com.ua/v4/goods/get?front-type=xl&country=UA&lang=ru&sort=expensive&category_id=80003');
+        $request = $client->get('https://xl-catalog-api.rozetka.com.ua/v4/goods/get?front-type=xl&country=UA&lang=ru&sort=expensive&category_id=80004');
         //        &page=3 если нужно увижеть страницу выше это мы ищем по категоии тут мы выбераем категорию
 
-        $response= json_decode($request->getBody()->getContents(),true);
+
+        $response = json_decode($request->getBody()->getContents(), true);
         //тут мы преобразуем наши данные через json... а ниже мы их переводим из массива в строку что бы подставить ее позже в нашой переменной requesItems
-        $myIdItems = (implode(',',$response['data']['ids']) );
+        $myIdItems = (implode(',', $response['data']['ids']));
 
 
+        //ниже мы выводим все наши продукты в данной категории
         $requestItems = $client->get("https://xl-catalog-api.rozetka.com.ua/v4/goods/getDetails?front-type=xl&country=UA&lang=ru&with_groups=1&with_docket=1&goods_group_href=1&product_ids=$myIdItems");
-        $responseItems= json_decode($requestItems->getBody()->getContents(),true);
-        dd($responseItems);
+        $responseItems = json_decode($requestItems->getBody()->getContents(), true);
+
+        //      dd($responseItems);
+//        foreach ($responseItems['data'] as $item) {
+//            $docket = $item['docket'];
+//            if (!empty($docket)) {
+//                dd($docket) ;
+//            }
+//        }
 
 
+        //Подключаемся к нашей базе данных
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "rozetkaparse";
+        // устанавливаем соединение с базой данных
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 
+        // проверяем соединение
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
 
 
-        //        $productTest = json_decode($request->getBody()->getContents(), true);
-        //        dd(implode(',',$productTest['data']['ids']) );
+        //тут должен быть форич
+        foreach ($responseItems['data'] as $item) {
+            $title = $item['title'];
+//            $description = $item['description'];
+            $price = $item['price'];
+            $image_url = $item['image_main'];
 
-        //        $request = $client->get('https://api31-core-useast1a.tiktokv.com/aweme/v1/feed/');
-        //        $response= json_decode($request->getBody()->getContents(),true);
-        //        dd($response);
-    }
+
+            $title = mysqli_real_escape_string($conn, $title);
+//            $description = mysqli_real_escape_string($conn, $description);
+            $image_url = mysqli_real_escape_string($conn, $image_url);
+
+
+            $sql = "INSERT INTO products (title, price, image_url) VALUES ('$title', '$price', '$image_url')";
+
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Product added successfully. <br>";
+            } else {
+                echo "Error adding product: " . $conn->error;
+            }
+
+            // добавили что бы не забанили
+            sleep(30);
+        }
+
+        //закрываем соединение с базой данных
+        mysqli_close($conn);
+  }
 }
